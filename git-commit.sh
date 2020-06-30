a1aa3cfd2d6f92a5b47a10a23e87565a3895af6a
root <root@ip-10-0-1-29.us-east-2.compute.internal>
2020-06-30 14:50:51 +0000

    new commit 1

--- git-commit.sh
+++ git-commit.sh
@@ -2,5 +2,5 @@
 git init
 git remote add polyglot-sockshop  https://github.com/thecloudgarage/polyglot-sockshop.git
 git add .
-git commit -m 'new commit'
+git commit -m 'new commit 1'
 git push -f polyglot-sockshop master
