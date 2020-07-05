e343672b4663219fda151f8905cd29ff0cff5b22
root <root@ip-10-0-1-206.us-east-2.compute.internal>
2020-07-05 13:23:53 +0000

    new commit 1

--- locust-testing/scripts/locustfile.py
+++ locust-testing/scripts/locustfile.py
@@ -18,10 +18,7 @@ class WebTasks(TaskSet):
         self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
         self.client.get("/category.html")
         self.client.get("/detail.html?id={}".format(item_id))
-        self.client.delete("/cart")
-        self.client.post("/cart", json={"id": item_id, "quantity": 1})
         self.client.get("/basket.html")
-        self.client.post("/orders")
 
 
 class Web(HttpUser):
